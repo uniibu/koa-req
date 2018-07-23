7910bc82f99f54865dcd76cabc72aafb639386b2
uni <unibtc@gmail.com>
2018-07-23 18:25:43 +0800

    uses @koa/cors and koa-helmet, added ipfix and koa-logger

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-module.exports = {
-  cors: require('./lib/kCors'),
-  helmet: require('./lib/kHelmet'),
-  methodOverride: require('./lib/kMethodOverride')
+const cors = require('@koa/cors');
+const helmet = require('koa-helmet');
+const ipFix = require('./lib/ipaddress');
+const logger = require('koa-logger');
+const defaultOPts = {
+  loggerEnabled: true,
+  corsEnabled: true,
+  helmetEnabled: true,
+  ipfixEnabled: true,
+  cors: {},
+  helmet: {}
+};
+module.exports = (app, opts = {}) => {
+  opts = Object.assign({}, defaultOPts, opts);
+  if (opts.ipfixEnabled) {
+    app.use(ipFix());
+  }
+  if (opts.loggerEnabled) {
+    app.use(logger());
+  }
+  if (opts.corsEnabled) {
+    app.use(cors(opts.cors));
+  }
+  if (opts.helmetEnabled) {
+    app.use(helmet(opts.helmet));
+  }
 };
